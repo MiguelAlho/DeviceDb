39c7032aa20cbc754043054758993c5c6d4242a5
Miguel alho <alho@miguelalho.com>
2021-10-08 18:22:34 +0100

    add Device Id value object and tests
    Current Device Id is guid based. Create() generates an id with a new Guid.

---
+++ src/DeviceDb.Api/Domain/Devices/DeviceId.cs
@@ -0,0 +1,25 @@
+﻿namespace DeviceDb.Api.Domain.Devices
+{
+    /// <summary>
+    /// Identifier for an individual device
+    /// 
+    /// Current schema is a based on a simple guid, but could be based on any other
+    /// RFC for IoT device identification if the domain warrants it.
+    /// </summary>
+    public record DeviceId
+    {        
+        internal Guid Value { get; }
+
+        public static DeviceId Create() => new DeviceId(Guid.NewGuid());
+
+        private DeviceId(Guid value) => Value = value;
+
+        internal static DeviceId From(Guid deviceId)
+        {
+            if (deviceId == default(Guid))
+                throw new ArgumentException("invalid device id format", nameof(deviceId));
+
+            return new DeviceId(deviceId);
+        }
+    }
+}
---
+++ test/DeviceDb.Api.Tests/Domain/Devices/DeviceIdTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Xunit;
+using FluentAssertions;
+using DeviceDb.Api.Domain.Devices;
+using AutoFixture;
+
+namespace DeviceDb.Api.Tests.Domain.Devices
+{
+    public class DeviceIdTests
+    {
+        static Fixture _fixture = new Fixture();
+
+        public class WhenCreatingAnId
+        {
+            private readonly Guid _idGuid;
+            private readonly DeviceId _generatedId;
+
+            public WhenCreatingAnId()
+            {
+                _idGuid = _fixture.Create<Guid>();
+                _generatedId = DeviceId.From(_idGuid);
+            }
+
+            [Fact]
+            public void ValueObjectIsGenerated() => _generatedId.Should().NotBeNull();
+
+            [Fact]
+            public void CanAccessInternalGuidValue() => _generatedId.Value.Should().Be(_idGuid);
+        }
+
+        public class InvariantsAreProtected
+        {
+            //non null inputg protected by non-nullable Guid
+
+            [Fact]
+            public void ExceptionIsThrownIfDeviceIdGuidIsEmpty()
+            {
+                Action act = () => DeviceId.From(Guid.Empty);
+                act.Should().Throw<ArgumentException>();
+            }
+
+            [Fact]
+            public void ExceptionIsThrownIfDeviceIdGuidIsDefault()
+            {
+                Action act = () => DeviceId.From(default);
+                act.Should().Throw<ArgumentException>();
+            }
+        }
+    }
+}
