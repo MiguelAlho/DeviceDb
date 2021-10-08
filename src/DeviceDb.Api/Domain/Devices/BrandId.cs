c1b5d860272509ce9d8b116e1c30bf89bbe0c597
Miguel alho <alho@miguelalho.com>
2021-10-08 18:11:08 +0100

    add brandId Value type
    BrandId is currently string based.
    
    Includes tests to verify construction from string and conversion and invariant protection.

---
+++ src/DeviceDb.Api/Domain/Devices/BrandId.cs
@@ -0,0 +1,22 @@
+﻿namespace DeviceDb.Api.Domain.Devices
+{
+    /// <summary>
+    /// Identifier for an individual brand
+    /// </summary>
+    public record BrandId
+    {
+        public string Value { get; }
+
+        private BrandId(string value) => Value = value;
+
+        internal static BrandId From(string brandId)
+        {
+            if (string.IsNullOrWhiteSpace(brandId) || brandId.Length > 100)
+                throw new ArgumentException("invalid brand id format", nameof(brandId));
+            
+            return new BrandId(brandId);
+        }
+
+        public override string ToString() => Value;
+    }
+}
---
+++ test/DeviceDb.Api.Tests/Domain/Devices/BrandIdTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+using FluentAssertions;
+using DeviceDb.Api.Domain.Devices;
+using AutoFixture;
+
+namespace DeviceDb.Api.Tests.Domain.Devices
+{
+    public class BrandIdTests
+    {
+        static Fixture _fixture = new Fixture();
+
+        public class WhenCreatingAnId
+        {
+            private readonly string _idString;
+            private readonly BrandId _generatedId;
+
+            public WhenCreatingAnId()
+            {
+                _idString = _fixture.Create<string>();
+                _generatedId = BrandId.From(_idString);
+            }
+           
+            [Fact]
+            public void ValueObjectIsGenerated() => _generatedId.Should().NotBeNull();
+
+            [Fact]
+            public void CanGenerateStringVersionAgain() => _generatedId.ToString().Should().Be(_idString);
+        }
+
+        public class InvariantsAreProtected
+        {
+            //non null inputg protected by non-nullable string
+
+            [Fact]
+            public void ExceptionIsThrownIfBrandIdStringIsEmpty()
+            {
+                Action act = () => BrandId.From(String.Empty);
+                act.Should().Throw<ArgumentException>();
+            }
+
+            [Fact]
+            public void ExceptionIsThrownIfBrandIdStringIsWhitespace()
+            {
+                Action act = () => BrandId.From("   ");
+                act.Should().Throw<ArgumentException>();
+            }
+
+            [Fact]
+            public void ExceptionIsThrownIfBrandIdStringIsLongerThen100Characters()
+            {
+                var longId = string.Join("", Enumerable.Repeat(0, 101).Select(n => (char)new Random().Next(127)));
+
+                Action act = () => BrandId.From(longId);
+                act.Should().Throw<ArgumentException>();
+            }
+        }
+    }
+}
