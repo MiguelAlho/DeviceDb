d3291823a14e6d7390d1193d736bee71fa2e7a32
Miguel alho <alho@miguelalho.com>
2021-10-09 21:54:24 +0100

    cleanup bad device test

--- test/DeviceDb.Api.Tests/Domain/Devices/DeviceTests.cs
+++ test/DeviceDb.Api.Tests/Domain/Devices/DeviceTests.cs
@@ -3,6 +3,7 @@ using Xunit;
 using FluentAssertions;
 using DeviceDb.Api.Domain.Devices;
 using AutoFixture;
+using FluentAssertions.Extensions;
 
 namespace DeviceDb.Api.Tests.Domain.Devices
 {
@@ -11,27 +12,18 @@ namespace DeviceDb.Api.Tests.Domain.Devices
         readonly Fixture _fixture = new();
 
         [Fact]
-        public void CanCreateInstanceOfDevice()
-        {
-            var device = new Device(DeviceId.Create(), string.Empty, BrandId.From(_fixture.Create<string>()), DateTime.Now);
-
-            device.Should().NotBeNull();
-        }
-
-        [Fact]
         public void CreateCreatesInstancesWithValidDate()
         {
             var id = DeviceId.Create();
             var name = _fixture.Create<string>();
             var brand = BrandId.From(_fixture.Create<string>());
 
-            //var device = new Device.Create(id, name, brand);
+            var device = Device.Create(id, name, brand);
 
-            //device.Should().NotBeNull();
-            //device.Id.Should().Be(id);
-            //device.Name.Should().Be(name);
-            //device.Brand.Should().Be(brand);
-            //device.CreatedOn.Should().BeCloseTo(DateTime.Now, 10.Seconds);
+            device.Id.Should().Be(id);
+            device.Name.Should().Be(name);
+            device.BrandId.Should().Be(brand);
+            device.CreatedOn.Should().BeCloseTo(DateTime.Now, 1.Seconds());
         }
     }
 }
